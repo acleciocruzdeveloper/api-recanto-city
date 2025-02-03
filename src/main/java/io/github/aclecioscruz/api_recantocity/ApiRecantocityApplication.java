46fb2fdb3718520887d8d04054321fa3697b5d8b
sonic-developer <aclecioscruz@gmail.com>
2025-02-03 11:59:02 -0300

    first commit

---
+++ src/main/java/io/github/aclecioscruz/api_recantocity/ApiRecantocityApplication.java
@@ -0,0 +1,13 @@
+package io.github.aclecioscruz.api_recantocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRecantocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRecantocityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aclecioscruz/api_recantocity/ApiRecantocityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aclecioscruz.api_recantocity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRecantocityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
